7d8ef5fdda04b06a6594b45cce707de45fe388cd
akifumiyamane <akifumiyamane@icloud.com>
2022-09-16 14:47:16 +0900

    ユーザー管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々-]/}
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々-]/}
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]/}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]/}
   validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
   validates :birthday, presence: true
