da7df915597326925e3ec3aa9e5556b71214304a
akifumiyamane <akifumiyamane@icloud.com>
2022-09-21 14:46:57 +0900

    商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def index
-    @items = Item.all
-    @items = Item.new
+   # @items = Item.all
+    #@items = Item.new
   end 
   
   def new
